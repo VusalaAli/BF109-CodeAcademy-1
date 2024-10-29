0305e6703aac606b688b08a5e20936a66e1b7e72
minaabdullayeva <minaabdullayeva125@gmail.com>
2024-10-29 01:28:52 -0800

    added

---
+++ JavaScript/index.js
@@ -0,0 +1,150 @@
+// let num = prompt("enter number value:");
+// if(num % 2 === 0){
+//     alert("even");
+// }else{
+//     alert("odd")
+// }
+
+
+// let gpa = prompt("enter your gpa");
+// if(gpa <=100 && gpa >=0){
+//     if(gpa <=100 && gpa >=90){
+//         alert("your gpa is A");
+//     }else if(gpa <=90 && gpa >=80){
+//         alert("your gpa is B");
+//     }else if(gpa <=80 && gpa >=70){
+//         alert("your gpa is C");
+//     }else if(gpa <=70 && gpa >=60){
+//         alert("your gpa is D");
+//     }else if(gpa <=60 && gpa >=50){
+//         alert("your gpa is E")
+//     }
+
+// }
+
+// let month = prompt("enter month name").toLowerCase();
+
+// switch(month){
+//     case "september":
+//     case "october":
+//     case "november":
+//         alert("Season is Autumn");
+//         break;
+
+//     case "december":
+//     case "january":
+//     case "february":
+//         alert("Season is Winter");
+//         break;
+
+//     case "mart":
+//     case "april":
+//     case "may":
+//         alert("Season is Spring");
+//         break;
+
+//     case "june":
+//     case "july":
+//     case "august":
+//         alert("Season is Summer");
+//         break;
+        
+
+//     default:
+//         alert("Please, enter correct month name")
+// }
+
+
+
+// let p = prompt("your age:");
+
+// if(p >=0 && p <125){
+//     if(p >65){
+//         alert("URAAAAA! pensiya alabilirsin, canimm")
+//     }else{
+//         alert("kul bashuvaa :)))")
+//     }
+// }else{
+//     alert("please, yasi duz yaz!!!")
+// }
+
+
+// let a = prompt("a=");
+// let b = prompt("b=");
+// let c = prompt("c=");
+
+// if(a > 0 && b > 0 && c > 0){
+//     if(a == b && b == c){
+//         alert("beraber");
+//     }else if(a == b && a != c || a == c && a != b || b == c && b != a){
+//         alert("beraberyanli");
+//     }else{
+//         alert("muxtelifterefli")
+//     }
+
+// }else{
+//     alert("eded musbet deyil")
+// }
+
+
+// let number = prompt("ededi daxil edin");
+
+// if(number >0){
+//     switch (number) {
+//         case "1":
+//             alert("january");
+//             break;
+//             case "2":
+//             alert("february");
+//             break;
+//             case "3":
+//             alert("march");
+//             break;
+//             case "4":
+//             alert("april");
+//             break;
+//             case "5":
+//             alert("may");
+//             break;
+//             case "6":
+//             alert("june");
+//             break;
+//             case "7":
+//             alert("july");
+//             break;
+//             case "8":
+//             alert("august");
+//             break;
+//             case "9":
+//             alert("september");
+//             break;
+//             case "10":
+//             alert("october");
+//             break;
+//             case "11":
+//             alert("nowember");
+//             break;
+//             case "12":
+//             alert("december");
+//             break;
+    
+//         default:
+            
+//             break;
+//     }
+// }else{
+//     alert("ededi duzgun yaz");
+// }
+
+
+let salary = prompt("emek haqqi:")
+let price = prompt("istediyiniz meblegh:")
+let time = prompt("istediyiniz muddet(ay):")
+let kredit;
+if(time == 36){
+    
+    
+}else{
+    alert("size bu shertlerle kredit dushmur...(");
+    console.log("size bu shertlerle kredit dushmur...(");
+}
